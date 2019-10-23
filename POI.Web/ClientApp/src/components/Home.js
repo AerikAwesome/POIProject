c990b6f5b6413109c2fa8fa86c508de7ffdafaa7
Erik Buter <e.t.buter@gmail.com>
2019-10-23 22:23:09 +0200

    Working map widget

--- POI.Web/ClientApp/src/components/Home.js
+++ POI.Web/ClientApp/src/components/Home.js
@@ -1,46 +1,65 @@
-import React, { Component } from 'react';
-import { Map, TileLayer, Marker, Popup } from '../../src'
+import React, { Component } from 'react'
+import { Map as LeafletMap, TileLayer, Marker, Popup } from 'react-leaflet'
 
-type State = {
-    lat: number,
-    lng: number,
-    zoom: number,
+
+const testData = [
+    { lat: 51.505, lng: -0.09, text: "This is a test marker" }
+];
+
+const MarkerList = (props) => (
+    <div>
+        {props.markers.map(marker => <MarkerWidget {...marker} />)}
+    </div>
+);
+
+class MarkerWidget extends Component {
+    render() {
+        const marker = this.props;
+        return (
+            <Marker position={[marker.lat, marker.lng]}>
+                <Popup>
+                    {marker.text}
+          </Popup>
+            </Marker>
+            );
+    }
 }
 
-export default class Home extends Component<{}, State> {
-    state = {
-        lat: 51.505,
-        lng: -0.09,
-    zoom: 13,
+class MapWidget extends Component
+{
+    constructor(props) {
+        super(props);
+        this.state = {
+            lat: 51.505,
+            lng: -0.09,
+            zoom: 13,
+            markers: testData
+        };
+    }
+
+    render() {
+        const position = [this.state.lat, this.state.lng];
+        return (
+            <LeafletMap
+                center={position}
+                zoom={this.state.zoom}>
+                <TileLayer attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png">
+                </TileLayer>
+                <MarkerList markers={this.state.markers}/>
+            </LeafletMap>
+        );
+    }
 }
+
+export class Home extends Component {
     static displayName = Home.name;
 
     render() {
-    const position = [this.state.lat, this.state.lng]
-    return (
-      <div>
-            <h1>Hello, world!</h1>
-            <Map center={position} zoom={this.state.zoom}>
-                <TileLayer attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png">
-                </TileLayer>
-        attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            </Map>
-        <p>Welcome to your new single-page application, built with:</p>
-        <ul>
-          <li><a href='https://get.asp.net/'>ASP.NET Core</a> and <a href='https://msdn.microsoft.com/en-us/library/67ef8sbd.aspx'>C#</a> for cross-platform server-side code</li>
-          <li><a href='https://facebook.github.io/react/'>React</a> for client-side code</li>
-          <li><a href='http://getbootstrap.com/'>Bootstrap</a> for layout and styling</li>
-        </ul>
-        <p>To help you get started, we have also set up:</p>
-        <ul>
-          <li><strong>Client-side navigation</strong>. For example, click <em>Counter</em> then <em>Back</em> to return here.</li>
-          <li><strong>Development server integration</strong>. In development mode, the development server from <code>create-react-app</code> runs in the background automatically, so your client-side resources are dynamically built on demand and the page refreshes when you modify any file.</li>
-          <li><strong>Efficient production builds</strong>. In production mode, development-time features are disabled, and your <code>dotnet publish</code> configuration produces minified, efficiently bundled JavaScript files.</li>
-        </ul>
-        <p>The <code>ClientApp</code> subdirectory is a standard React application based on the <code>create-react-app</code> template. If you open a command prompt in that directory, you can run <code>npm</code> commands such as <code>npm test</code> or <code>npm install</code>.</p>
-      </div>
-    );
-  }
+        return (
+            <div>
+                <MapWidget />
+            </div>
+        );
+    }
 }
